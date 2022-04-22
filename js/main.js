23697f2d695ebea4585b3d9503534502143227a2
Brian Barquesi <barquesi.tech@gmail.com>
2022-04-22 09:14:22 -0300

    reutilizando funcion para crear elementos en la pagina

--- js/main.js
+++ js/main.js
@@ -1,76 +1,85 @@
 const sectorDeContactos = document.getElementById('redes-sociales')     // Traemos a una variable el elemento del DOM identificado por el id redes-sociales
 const sectorDeSkills = document.getElementById('sector-skills') 
-function crearElementoContacto(parametro){                              // Creamos una función que crea el elemento tal cual queremos esté en el DOM    
-    return `<img class="img-social" src=${parametro.img}
-    alt=""><a href=""></a>${parametro.nombre}`
-}
-function crearElementoSkills(parametro){                              // Creamos una función que crea el elemento tal cual queremos esté en el DOM    
-    return `<img class="img-skills" src=${parametro.img}
+function crearElemento(parametro){                              // Creamos una función que crea el elemento tal cual queremos esté en el DOM    
+    return `<img class=${parametro.class} src=${parametro.img}
     alt=""><a href="">${parametro.nombre}</a>`
 }
+
 const redes = [                                                         // Creamos la lista con los datos de cada elemento a crear 
     {
         "nombre":"GitHub",
-        "img":"assets/img/icons/github.svg"
+        "img":"assets/img/icons/github.svg",
+        "class":"img-social"
     },
     {
         "nombre":"LinkedIn",
-        "img":"assets/img/icons/linkedin.svg"
+        "img":"assets/img/icons/linkedin.svg",
+        "class":"img-social"
     },
     {
         "nombre":"Instagram",
-        "img": "assets/img/icons/instagram.svg"
+        "img": "assets/img/icons/instagram.svg",
+        "class":"img-social"
     },
     {
         "nombre":"Curriculum",
-        "img": "assets/img/icons/curriculum-vitae.svg"
+        "img": "assets/img/icons/curriculum-vitae.svg",
+        "class":"img-social"
     }
 ]
 const skills = [
     {
         "nombre": "JavaScript",
         "img":"assets/img/skills/javascript-programming-language.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"HTML",
-        "img":"assets/img/skills/html.svg"
+        "img":"assets/img/skills/html.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"CSS3",
-        "img":"assets/img/skills/css.svg"
+        "img":"assets/img/skills/css.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"Python",
-        "img":"assets/img/skills/python-programming-language.svg"
+        "img":"assets/img/skills/python-programming-language.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"Git",
-        "img":"assets/img/skills/git.svg"
+        "img":"assets/img/skills/git.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"Flutter",
-        "img":"assets/img/skills/flutter.svg"
+        "img":"assets/img/skills/flutter.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"Conocimiento sobre redes",
-        "img":"assets/img/skills/cloud-server.svg"
+        "img":"assets/img/skills/cloud-server.svg",
+        "class":"img-skills"
     },
     {
         "nombre":"Reparación de equipos informáticos",
-        "img":"assets/img/skills/workstation.svg"
+        "img":"assets/img/skills/workstation.svg",
+        "class":"img-skills"
     },
 ]
 
 redes.forEach(contacto => {                                             // Por cada elemente en el Array de redes
     let li = document.createElement('li')                               // va a crear un elemento de tipo li
-    li.innerHTML = crearElementoContacto(contacto)                      // va a agregar el resuitado de la función crearEllemento
+    li.innerHTML = crearElemento(contacto)                      // va a agregar el resuitado de la función crearEllemento
     sectorDeContactos.appendChild(li)                                   // y finalmente lo agrega como hijo del sector de contactos en el DOM 
 });
 
 skills.forEach(skill =>{
     let li = document.createElement('li')
     li.classList.add('lista-skills')
-    li.innerHTML = crearElementoSkills(skill)
+    li.innerHTML = crearElemento(skill)
     sectorDeSkills.appendChild(li)
 } )
 
