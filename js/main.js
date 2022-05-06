7571e566bd423a76f123ee20b398f8bb2033cf8f
Brian Barquesi <barquesi.tech@gmail.com>
2022-05-05 23:46:09 -0300

    agregando formaciones

--- js/main.js
+++ js/main.js
@@ -42,10 +42,13 @@ async function crearElementosHobbies(resource) {
 
 function elementoFormacion(parametro){                              // Creamos una función que crea el elemento tal cual queremos esté en el DOM    
     return `
-    <p class="subtitulo">${parametro.nombre}</p>
-    <p class="subtitulo-2">${parametro.institucion}</p>
-    <p>${parametro.duracion}</p>
-    <img class=${parametro.class} src=${parametro.img}alt=""><p href="#">
+    <img class="${parametro.class}" src=${parametro.img} alt="">
+    <div class="itemFormacion">
+        <p class="subtitulo">${parametro.nombre}</p>
+        <p class="subtitulo-2">${parametro.institucion}</p>
+        <p>${parametro.duracion}</p>
+        <p>${parametro.description}</p>
+    </div>
     `
 }
 
