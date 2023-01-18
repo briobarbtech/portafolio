817a36006e98bdf52612466d9ad2e6cc9ef629c0
Brian Barquesi <barquesi.tech@gmail.com>
2023-01-18 11:21:05 -0300

    .

--- assets/endpoints.js
+++ assets/endpoints.js
@@ -1,4 +1,4 @@
 class Endpoint {
-    static urlRemote = `https://briobarbtech.github.io/portafolio/db.json`;
+    static urlRemote = `https://briobarbtech.github.io/brian-barquesi-portafolio/db.json`;
     static urlLocal = '../../db.json'
 }
